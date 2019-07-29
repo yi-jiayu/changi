1559a257238934799d95a976fdbf9ceea165b5e5
Jiayu Yi <yijiayu@gmail.com>
2019-07-29 12:06:05 +0530

    Initial commit

---
+++ lib/departures.ex
@@ -0,0 +1,18 @@
+defmodule Departures do
+  @moduledoc """
+  Documentation for Departures.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Departures.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/departures_test.exs
@@ -0,0 +1,8 @@
+defmodule DeparturesTest do
+  use ExUnit.Case
+  doctest Departures
+
+  test "greets the world" do
+    assert Departures.hello() == :world
+  end
+end
