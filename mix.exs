d94970cc6ab20148ca61b954d79483d44b557dc2
Jiayu Yi <yijiayu@gmail.com>
2019-08-21 23:04:01 +0800

    Add :eex to extra_applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Changi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :eex],
       mod: {Changi.Application, []}
     ]
   end
