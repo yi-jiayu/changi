d04d8fc50b24704b0f2818cd3dbe6ee1abebafc6
Jiayu Yi <yijiayu@gmail.com>
2019-08-23 15:46:51 +0800

    Show estimated arrival/departure time

--- test/changi_test.exs
+++ test/changi_test.exs
@@ -9,8 +9,25 @@ defmodule ChangiTest do
   @arrival File.read!("arrival.json") |> Jason.decode!()
   @departure File.read!("departure.json") |> Jason.decode!()
   describe "format_flight/1" do
-    test "format arrival" do
+    test "format arrival (with estimated time)" do
       assert Changi.format_flight(@arrival, "3K520") == """
+             *3K520*
+             JETSTAR ASIA
+             Arriving from Bangkok (Suvarnabhumi)
+             Scheduled time: 20190730 15:10:00
+             Estimated time: 14:46
+
+             Status: *Confirmed*
+
+             T1
+             Luggage at belt 10
+             """
+    end
+
+    test "format arrival (without estimated time)" do
+      arrival = %{@arrival | "estimationTimeFlag" => false, "estimatedTime" => ""}
+
+      assert Changi.format_flight(arrival, "3K520") == """
              *3K520*
              JETSTAR ASIA
              Arriving from Bangkok (Suvarnabhumi)
@@ -23,8 +40,26 @@ defmodule ChangiTest do
              """
     end
 
-    test "format departure" do
+    test "format departure (with estimated time)" do
       assert Changi.format_flight(@departure, "SQ318") == """
+             *SQ318*
+             SINGAPORE AIRLINES
+             Destination: London
+             Scheduled time: 20190730 12:35:00
+             Estimated time: 12:55
+
+             Status: *Departed*
+
+             T3
+             Check-in at row 04 (door 3)
+             Board at gate A14
+             """
+    end
+
+    test "format departure (without estimated time)" do
+      departure = %{@departure | "estimationTimeFlag" => false, "estimatedTime" => ""}
+
+      assert Changi.format_flight(departure, "SQ318") == """
              *SQ318*
              SINGAPORE AIRLINES
              Destination: London
@@ -45,6 +80,7 @@ defmodule ChangiTest do
              Operated by: SINGAPORE AIRLINES SQ318
              Destination: London
              Scheduled time: 20190730 12:35:00
+             Estimated time: 12:55
 
              Status: *Departed*
 
